afb21e11fadfe88e997e2ef7dbf516271e3d6921
shamiul <anik.savar.bd@gmail.com>
2023-06-14 16:18:37 +0600

    AddClass page design and functionality update

--- src/pages/Dashboard/Instructor/AddClass/AddClass.jsx
+++ src/pages/Dashboard/Instructor/AddClass/AddClass.jsx
@@ -2,13 +2,49 @@ import AddClassImage from '../../../../assets/images/language-banner.jpg';
 import { Fade } from "react-awesome-reveal";
 import { AuthContext } from '../../../../providers/AuthProvider';
 import { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const AddClass = () => {
 
   const { user } = useContext(AuthContext);
 
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
   const currentUserName = user?.displayName;
   const currentUserEmail = user?.email;
+
+  const onSubmit = (classInfo) => { 
+    // console.log(classInfo);
+
+    const classDetails = {
+      class_name: classInfo.class_name,
+      class_image: classInfo.class_image,
+      instructor_name: classInfo.instructor_name,
+      instructor_email: classInfo.instructor_email,
+      available_seats: parseFloat(classInfo.available_seats),
+      enrolled_students: parseFloat(0),
+      class_price: parseFloat(classInfo.price),
+      class_status: "pending"
+    }
+    console.log(classDetails);
+    if(user && user.email) {
+      axios.post(`${import.meta.env.VITE_API_URL}/add-a-class`, classDetails)
+        .then((data) => {
+          console.log(data.data);
+          if (data?.data?.insertedId) {
+            Swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: 'Class added successfully!',
+              showConfirmButton: true,
+              timer: 3000
+            })
+          }
+        });
+    }
+  };
 
   return (
     <>
@@ -29,100 +65,60 @@ const AddClass = () => {
 
           <div className='border-t border-slate-300 my-2 mx-6 md:mx-8'></div>
 
-          {/* {error && <p className="!px-6 md:!px-8 text-red-500 mt-2 text-center">{error}</p>} */}
-          {/* {success && <p className="!px-6 md:!px-8 text-green-600 mt-2 text-center">{success}</p>} */}
-
-          <form >
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="!px-6 md:!px-8 !pt-2 card-body">
               <div className="form-control">
                 <label className="label pl-0" htmlFor="className">
                   <span className="label-text text-md md:text-[16px]">Class Name</span>
                 </label>
-                <input type="text" id="className" name="class_name" placeholder="Enter class name" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" />
-                {/* <input type="text" id="toyName" name="toy_name" placeholder="Enter toy name" className={toyNameError ? "input input-error input-sm py-5 text-[14px] focus:outline-red-200 focus:ring-red-500 focus:border-red-500" : "input input-bordered input-sm py-5 text-[14px]"} /> */}
-                {/* {toyNameError && <p className="text-red-500 mt-2">{toyNameError}</p>} */}
+                <input type="text" id="className" {...register("class_name", { required: true })} name="class_name" placeholder="Enter class name" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" />
+                {errors?.class_name && <p className="text-red-500 mt-2">Class name is required!</p>} {/* Error Message */}
               </div>
 
               <div className="form-control">
-                <label className="label pl-0" htmlFor="photoURL">
+                <label className="label pl-0" htmlFor="classImage">
                   <span className="label-text text-md md:text-[16px]">Class Image</span>
                 </label>
-                <input type="text" id="photoURL" name="photo_url" placeholder="Enter class image" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" />
-                {/* <input type="text" id="photoURL" name="photo_url" placeholder="Enter toy's photo url" className={photoURLError ? "input input-error input-sm py-5 text-[14px] focus:outline-red-200 focus:ring-red-500 focus:border-red-500" : "input input-bordered input-sm py-5 text-[14px]"} /> */}
-                {/* {photoURLError && <p className="text-red-500 mt-2">{photoURLError}</p>} */}
+                <input type="text" id="classImage" {...register("class_image", { required: true })} name="class_image" placeholder="Enter class image" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" />
+                {errors?.class_image && <p className="text-red-500 mt-2">Photo url is required!</p>} {/* Error Message */}
               </div>
 
               <div className="form-control">
                 <label className="label pl-0" htmlFor="instructorName">
                   <span className="label-text text-md md:text-[16px]">Instructor Name</span>
                 </label>
-                <input type="text" id="instructorName" name="instructor_name" placeholder="Instructor's name" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" defaultValue={currentUserName} readOnly />
-                {/* <input type="text" id="sellerName" name="seller_name" placeholder="Instructor's name" className="input input-bordered input-sm py-5 text-[14px]" defaultValue={currentUserName} readOnly /> */}
+                <input type="text" id="instructorName" {...register("instructor_name", { required: true })} name="instructor_name" placeholder="Instructor's name" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" defaultValue={currentUserName} readOnly />
+                {errors?.instructor_name && <p className="text-red-500 mt-2">Instructor name is required!</p>} {/* Error Message */}
               </div>
 
               <div className="form-control">
                 <label className="label pl-0" htmlFor="instructorEmail">
                   <span className="label-text text-md md:text-[16px]">Instructor Email</span>
                 </label>
-                <input type="email" id="instructorEmail" name="instructor_email" placeholder="Instructor's email address" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" defaultValue={currentUserEmail} readOnly />
-                {/* <input type="email" id="sellerEmail" name="seller_email" placeholder="Instructor's email address" className={sellerEmailError ? "input input-error input-sm py-5 text-[14px] focus:outline-red-200 focus:ring-red-500 focus:border-red-500" : "input input-bordered input-sm py-5 text-[14px]"} defaultValue={currentUserEmail} readOnly /> */}
-                {/* {sellerEmailError && <p className="text-red-500 mt-2">{sellerEmailError}</p>} */}
+                <input type="email" id="instructorEmail" {...register("instructor_email", { required: true })} name="instructor_email" placeholder="Instructor's email address" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" defaultValue={currentUserEmail} readOnly />
+                {errors?.instructor_email && <p className="text-red-500 mt-2">Instructor email is required!</p>} {/* Error Message */}
               </div>
 
               <div className="grid gap-x-4 gap-y-2 md:grid-cols-2">
-                
-
-                {/* <div className="form-control">
-                <label className="label pl-0" htmlFor="subCategory">
-                  <span className="label-text text-md md:text-[16px]">Sub-category</span>
-                </label>
-                <select id="subCategory" name="sub_category" className={subCategoryError ? "select select-bordered border-red-500 select-sm min-h-[42px] leading-tight !text-[14px] !font-normal focus:outline-red-200 focus:ring-red-500 focus:border-red-500" : "select select-bordered select-sm min-h-[42px] leading-tight !text-[14px] !font-normal"}>
-                  <option value="">Choose sub-category</option>
-                  <option value="Language Toys">Language Toys</option>
-                  <option value="Math Toys">Math Toys</option>
-                  <option value="Science Toys">Science Toys</option>
-                </select>
-                {subCategoryError && <p className="text-red-500 mt-2">{subCategoryError}</p>}
-              </div> */}
-
                 <div className="form-control">
                   <label className="label pl-0" htmlFor="availableSeats">
                     <span className="label-text text-md md:text-[16px]">Available Seats</span>
                   </label>
-                  <input type="text" id="availableSeats" name="available_seats" placeholder="Enter number of available seats" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" />
-                  {/* <input type="text" id="rating" name="rating" placeholder="Enter toy's rating" className={ratingError ? "input input-error input-sm py-5 text-[14px] focus:outline-red-200 focus:ring-red-500 focus:border-red-500" : "input input-bordered input-sm py-5 text-[14px]"} /> */}
-                  {/* {ratingError && <p className="text-red-500 mt-2">{ratingError}</p>} */}
+                  <input type="text" id="availableSeats" {...register("available_seats", { required: true, pattern: { value: /^(\d+\.?\d*|\.\d+)$/ } })} name="available_seats" placeholder="Enter number of available seats" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" />
+                  {errors?.available_seats?.type === 'required' && <p className="text-red-500 mt-2">Available seats are required!</p>}
+                  {errors?.available_seats?.type === 'pattern' && <p className="text-red-500 mt-2">Available seats value must be a number!</p>}
                 </div>
 
                 <div className="form-control">
                   <label className="label pl-0" htmlFor="price">
                     <span className="label-text text-md md:text-[16px]">Price</span>
                   </label>
-                  <input type="text" id="price" name="price" placeholder="Enter price" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" />
-                  {/* <input type="text" id="price" name="price" placeholder="Enter toy's price" className={priceError ? "input input-error input-sm py-5 text-[14px] focus:outline-red-200 focus:ring-red-500 focus:border-red-500" : "input input-bordered input-sm py-5 text-[14px]"} /> */}
-                  {/* {priceError && <p className="text-red-500 mt-2">{priceError}</p>} */}
+                  <input type="text" id="price" {...register("price", { required: true, pattern: { value: /^(\d+\.?\d*|\.\d+)$/ } })} name="price" placeholder="Enter price" className="input input-bordered input-sm py-5 text-[14px] focus:ring-teal-400 focus:border-teal-400 focus:outline-teal-300" />
+                  {errors?.price?.type === 'required' && <p className="text-red-500 mt-2">Price is required!</p>} {/* Error Message */}
+                  {errors?.price?.type === 'pattern' && <p className="text-red-500 mt-2">Price value must be a number!</p>}
                 </div>
-
-                {/* <div className="form-control">
-                  <label className="label pl-0" htmlFor="quantity">
-                    <span className="label-text text-md md:text-[16px]">Quantity</span>
-                  </label>
-                  <input type="text" id="quantity" name="quantity" placeholder="Enter available quantity" className="input input-bordered input-sm py-5 text-[14px]" />
-                  <input type="text" id="quantity" name="quantity" placeholder="Enter available quantity" className={quantityError ? "input input-error input-sm py-5 text-[14px] focus:outline-red-200 focus:ring-red-500 focus:border-red-500" : "input input-bordered input-sm py-5 text-[14px]"} />
-                  {quantityError && <p className="text-red-500 mt-2">{quantityError}</p>}
-                </div> */}
               </div>
-
-              {/* <div className="form-control">
-              <label className="label pl-0" htmlFor="description">
-                <span className="label-text text-md md:text-[16px]">Description</span>
-              </label>
-              <textarea id="description" name="description" rows="6" className={descriptionError ? "input input-error input-sm py-5 text-[14px] h-auto focus:outline-red-200 focus:ring-red-500 focus:border-red-500" : "input input-bordered text-[14px] h-auto"} placeholder="Enter detail description"></textarea>
-              {descriptionError && <p className="text-red-500 mt-2">{descriptionError}</p>}
-            </div> */}
-
               <div className="form-control mt-3">
-                {/* <button type="submit" className="flex gap-3 mx-auto md:mx-0 w-full items-center justify-center text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-semibold rounded-lg text-lg px-8 py-2 text-center disabled:from-slate-300 disabled:to-slate-400 disabled:text-slate-600 tooltip tooltip-bottom">Add Toy</button> */}
                 <button type="submit" className="text-white bg-gradient-to-br from-teal-500 to-teal-600 ring-2 ring-offset-1 ring-teal-500 transition-all hover:duration-300 hover:from-teal-600 hover:to-teal-700 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-blue-200 dark:focus:ring-teal-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center">Add Class</button>
               </div>
             </div>
